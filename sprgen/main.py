dd90e32fcb268d877412373ee83b09f82d5ccd14
Brett <brettbar@live.com>
2022-06-06 10:41:29 -0700

    nearing completion on sprgen

--- sprgen/main.py
+++ sprgen/main.py
@@ -27,186 +27,190 @@ and the color of Reference.
 # ASSET_PATH = "C:\\Users\\brett\\Nextcloud\\projects\\gameart\\Aseprite\\Units\\Generator\\"
 
 # Linux
-ASSET_PATH = "/home/brettbar/nextcloud/projects/gameart/Aseprite/Units/Generator/"
+# ASSET_PATH = "/home/brettbar/nextcloud/projects/gameart/Aseprite/Units/Generator/"
+
+# WSL
+ASSET_PATH = "/mnt/c/Users/brett/Nextcloud/projects/gameart/Aseprite/Units/Generator/"
 
 def main():
-    gen_color_map()
-    gen_finished_anims()
+    gen_color_maps() 
+    gen_final_anims('Hastati') 
+    gen_final_anims('SenoneWarrior')
 
-def gen_finished_anims():
-    color_anims = Image.open(ASSET_PATH + 'MaleAnimations.png').convert('RGBA')
-    color_map = Image.open(ASSET_PATH + 'MaleColorMap.png').convert('RGBA')
-    reference = Image.open(ASSET_PATH + 'GreekVillager.png').convert('RGBA')
+def gen_final_anims(sprite_folder):
+    # 128 x 128 Color Maps for Sprite
+    color_maps = {
+        # 'Cape': Image.open(ASSET_PATH + 'TemplateLayers/Cape.png'),
+        'Legs': Image.open(ASSET_PATH + 'ColorMaps/Legs.png').convert('RGBA'),
+        'Torso': Image.open(ASSET_PATH + 'ColorMaps/Torso.png').convert('RGBA'),
+        'LeftArm': Image.open(ASSET_PATH + 'ColorMaps/LeftArm.png').convert('RGBA'),
+        'Head': Image.open(ASSET_PATH + 'ColorMaps/Head.png').convert('RGBA'),
+        'LeftEquip': Image.open(ASSET_PATH + 'ColorMaps/LeftEquip.png').convert('RGBA'),
+        'RightEquip': Image.open(ASSET_PATH + 'ColorMaps/RightEquip.png').convert('RGBA'),
+        'RightArm': Image.open(ASSET_PATH + 'ColorMaps/RightArm.png').convert('RGBA'),
+    }
 
-    final_anims = color_anims.copy()
+    # 128 x 128 Source Maps for Sprite
+    source_maps = {
+        'Legs': Image.open(ASSET_PATH + sprite_folder + '/Legs.png').convert('RGBA'),
+        'Torso': Image.open(ASSET_PATH + sprite_folder + '/Torso.png').convert('RGBA'),
+        'LeftArm': Image.open(ASSET_PATH + sprite_folder + '/LeftArm.png').convert('RGBA'),
+        'Head': Image.open(ASSET_PATH + sprite_folder + '/Head.png').convert('RGBA'),
+        'LeftEquip': Image.open(ASSET_PATH + sprite_folder + '/LeftEquip.png').convert('RGBA'),
+        'RightEquip': Image.open(ASSET_PATH + sprite_folder + '/RightEquip.png').convert('RGBA'),
+        'RightArm': Image.open(ASSET_PATH + sprite_folder + '/RightArm.png').convert('RGBA'),
+    }
 
-    anims_width, anims_height = color_anims.size
-    map_width, map_height =  color_map.size
-
-    top_left_color_anims_pixels = []
-    top_left_color_map_pixels = []
-
-
-    # Pixel space
-    # TODO only top left atm
-    for anims_x in range(0, anims_width -1):
-        for anims_y in range(0, anims_height -1):
-            r, g, b, a = color_anims.getpixel((anims_x, anims_y))
-
-            if a > 0: 
-                # print("Adding pixel", (anims_x, anims_y), (r, g, b, a))
-                top_left_color_anims_pixels.append((anims_x, anims_y))
-
-
-    for map_x in range(0, math.floor(map_width /2) -1):
-        for map_y in range(0, math.floor(map_height /2) -1):
-            _, _, _, a = color_map.getpixel((map_x, map_y))
-
-            if a > 0: top_left_color_map_pixels.append((map_x, map_y))
-    
-
-
-    for anims_pixel in top_left_color_anims_pixels:
-        anims_color = color_anims.getpixel(anims_pixel)
-
-
-        for map_pixel in top_left_color_map_pixels:
-            map_color = color_map.getpixel(map_pixel)
-
-            
-            if map_color == anims_color:
-                matching_pixel = map_pixel
-
-                final_anims.putpixel(anims_pixel, reference.getpixel(matching_pixel))
-
-            
-    final_anims.save(ASSET_PATH + "MaleFinalAnimations.png")
-            
-                    
-
-
-def gen_color_map():
-
-    template_image = Image.open(ASSET_PATH + 'MaleTemplate.png')
-
-    template_pic = template_image.convert('RGBA')
-    r, _, _, _ = template_image.split()
-    width, height = template_pic.size
-
-    if not width == template_pic.size[0] or not height == template_pic.size[1]:
-        print("Incompatible files")
-        return
-
-    # hues = {
-    #     "right_arm": (250, 0, 0),
-    #     "head": (225, 0, 0),
-    #     "left_arm": (200, 0, 0),
-    #     "torso": (175, 0, 0),
-    #     "right_leg": (150, 0, 0),
-    #     "left_leg": (125, 0, 0),
-    #     "right_equip": (100, 0, 0),
-    #     "left_equip": (75, 0, 0),
-    #     "cape": (50, 0, 0),
-    #     "mount": (25, 0, 0),
-    #     #"shadow": (70, 0, 0),
-    # }
-
-    
-    sprites = {
-        "TopLeft": {
-            "right_arm": [],
-            "head": [],
-            "left_arm": [],
-            "torso": [],
-            "right_leg": [],
-            "left_leg": [],
-            "right_equipped": [],
-            "left_equip": [],
-            "cape": [],
-            "mount": [],
-        },
-        "TopRight": {
-            "right_arm": [],
-            "head": [],
-            "left_arm": [],
-            "torso": [],
-            "right_leg": [],
-            "left_leg": [],
-            "right_equipped": [],
-            "left_equip": [],
-            "cape": [],
-            "mount": [],
-        }
+    # Entire Animation Spritesheet, separated by Layer
+    color_anims = {
+        'Legs': Image.open(ASSET_PATH + 'ColorAnims/Legs.png').convert('RGBA'),
+        'Torso': Image.open(ASSET_PATH + 'ColorAnims/Torso.png').convert('RGBA'),
+        'LeftArm': Image.open(ASSET_PATH + 'ColorAnims/LeftArm.png').convert('RGBA'),
+        'Head': Image.open(ASSET_PATH + 'ColorAnims/Head.png').convert('RGBA'),
+        'LeftEquip': Image.open(ASSET_PATH + 'ColorAnims/LeftEquip.png').convert('RGBA'),
+        'RightEquip': Image.open(ASSET_PATH + 'ColorAnims/RightEquip.png').convert('RGBA'),
+        'RightArm': Image.open(ASSET_PATH + 'ColorAnims/RightArm.png').convert('RGBA'),
     }
 
 
-    
-    for y in range(0, height -1):
+    # Entire Animation Spritesheet
+    final_anims = Image.open(ASSET_PATH + 'Test.png').convert('RGBA')
+    final_anims_dict = dict()
+
+    print("Generating Sprite:", sprite_folder)
+
+    # For each Animation Color Map Layer
+    for body_part, color_anim in color_anims.items():
+        print("Evaluating AnimMap:", body_part)
+        width, height = color_anim.size
+
+        # All the visible pixels for a given layer in the entire animation sheet
+        anim_pixels = []
         for x in range(0, width -1):
-            r, g, b, a = template_pic.getpixel((x,y))
+            for y in range(0, height -1):
+                _,_,_,a = color_anim.getpixel((x,y))
+               
+                if a == 0: continue
 
-            if a == 0: continue
-
-            direction = ""
-            if x < 128:
-                direction = "TopLeft"
-            elif x >= 128:
-                direction = "TopRight"
-
-            if r == 255 and g == 255 and b == 255:
-                sprites[direction]["right_arm"].append((x, y))
-            elif r == 255 and g == 0 and b == 0:
-                sprites[direction]["head"].append((x, y))
-            elif r == 255 and g == 128 and b == 0:
-                sprites[direction]["left_arm"].append((x, y))
-            elif r == 255 and g == 255 and b == 0:
-                sprites[direction]["torso"].append((x, y))
-            elif r == 0 and g == 255 and b == 0:
-                sprites[direction]["right_leg"].append((x, y))
-            elif r == 0 and g == 0 and b == 255:
-                sprites[direction]["left_leg"].append((x, y))
-            elif r == 128 and g == 255 and b == 255:
-                sprites[direction]["right_equip"].append((x, y))
-            elif r == 255 and g == 128 and b == 255:
-                sprites[direction]["left_equip"].append((x, y))
-            elif r == 128 and g == 128 and b == 128:
-                sprites[direction]["cape"].append((x, y))
-            elif r == 255 and g == 128 and b == 128:
-                sprites[direction]["mount"].append((x, y))
-
-    for sprite in sprites.values():
-        for body_part in sprite.values():
-
-            if len(body_part) <= 0: continue
+                anim_pixels.append((x, y))
 
 
-            prev_x, prev_y = body_part[0]
+        # All the visible pixels for a given color map
+        color_map_pixels = []
+        cm_width, cm_height = color_maps[body_part].size
+        for x in range(0, cm_width -1):
+            for y in range(0, cm_height -1):
+                _,_,_,a = color_maps[body_part].getpixel((x,y))
+               
+                if a == 0: continue
 
-            for x,y in body_part:
-                (pr, pg, pb, pa) = template_pic.getpixel((prev_x, prev_y))
+                color_map_pixels.append((x, y))
 
-                pr -= 2
-                pg -= 2
-                pb -= 2
+        # All the visible pixels for a given source map
+        source_map_pixels = []
+        sm_width, sm_height = source_maps[body_part].size
+        for x in range(0, sm_width -1):
+            for y in range(0, sm_height -1):
+                _,_,_,a = source_maps[body_part].getpixel((x,y))
+               
+                if a == 0: continue
 
-                # if x > prev_x: # we moved a column over
-                #     pr -= 1
-                #     pg -= 2
+                source_map_pixels.append((x, y))
 
-                # if y > prev_y:  # we moved a row down
-                #     pr -= 1
-                #     pb -= 2
+                
+        # For each pixel in the entire animation sheet for one layer
+        for anim_pixel in anim_pixels:
+
+            # Get the pixels color
+            anim_color = color_anim.getpixel(anim_pixel)
+
+            # Find the position of that color in the color_map 
+            lookup_pixel = (-1, -1)
+            for map_pixel in color_map_pixels:
+                map_color = color_maps[body_part].getpixel(map_pixel)
+
+                if anim_color == map_color:
+                    lookup_pixel = map_pixel
+                    break
+
+            # if lookup_pixel == (-1, -1):
+            #     print("ERROR IN COLOR MAP LOOKUP")
+
+            # With the position in the color map, find the color of that pixel in the source map
+            for source_pixel in source_map_pixels:
+                if source_pixel == lookup_pixel:
+                    final_anims_dict[anim_pixel] = True
+                    final_anims.putpixel(anim_pixel, source_maps[body_part].getpixel(lookup_pixel))
+                    break
+
+            
+    for x in range(0, final_anims.size[0]):
+        for y in range(0, final_anims.size[1]):
+            if (x, y) in final_anims_dict:
+                continue
+            else:
+                final_anims.putpixel((x, y), (0, 0, 0, 0))
+
+                
+
+            
+
+
+
+
                     
-                template_pic.putpixel((x, y), (pr, pg, pb, pa))
-
-                prev_x = x
-                prev_y = y
-                 
-
-    # picture.putalpha(alpha)
-    template_pic.save(ASSET_PATH + "MaleColorMap.png")
+    final_anims.save(ASSET_PATH + sprite_folder + "/FinalAnims.png")
 
 
+
+
+def gen_color_maps():
+    color_template_maps = {
+        # 'Cape': Image.open(ASSET_PATH + 'TemplateLayers/Cape.png'),
+        'Legs': Image.open(ASSET_PATH + 'TemplateLayers/Legs.png').convert('RGBA'),
+        'Torso': Image.open(ASSET_PATH + 'TemplateLayers/Torso.png').convert('RGBA'),
+        'LeftArm': Image.open(ASSET_PATH + 'TemplateLayers/LeftArm.png').convert('RGBA'),
+        'Head': Image.open(ASSET_PATH + 'TemplateLayers/Head.png').convert('RGBA'),
+        'LeftEquip': Image.open(ASSET_PATH + 'TemplateLayers/LeftEquip.png').convert('RGBA'),
+        'RightEquip': Image.open(ASSET_PATH + 'TemplateLayers/RightEquip.png').convert('RGBA'),
+        'RightArm': Image.open(ASSET_PATH + 'TemplateLayers/RightArm.png').convert('RGBA'),
+    }
+
+    for part, image in color_template_maps.items():
+        new_color_map = image.copy()
+        width, height = image.size
+
+        pixels_arr = []
+        for x in range(0, width -1):
+            for y in range(0, height -1):
+                _,_,_,a = image.getpixel((x,y))
+               
+                if a == 0: continue
+
+                new_color_map.putpixel((x,y), (0, 0, 0, 255))
+
+                pixels_arr.append((x, y))
+
+
+        prev_pixel = pixels_arr[0]
+        for pixel in pixels_arr:
+            pr, pg, pb, a = new_color_map.getpixel(prev_pixel)
+
+            if pr < 255:
+                pr += 2
+            elif pg < 255:
+                pg += 2
+            elif pb < 255:
+                pb += 2
+            else:
+                print("Exceeded maximum color combinations")
+
+            new_color_map.putpixel(pixel, (pr, pg, pb, a))
+
+            prev_pixel = pixel
+
+
+        new_color_map.save(ASSET_PATH + "ColorMaps/" + part + ".png")
 
 
 if __name__ == "__main__":
--- sprgen/old.py
+++ sprgen/old.py
@@ -1,21 +1,216 @@
-from PIL import Image, ImageDraw
+#!/usr/bin/env python
+
+from PIL import Image
+
+import math
 
 
-picture = Image.open('temp.png')
-width, height = picture.size
+
+'''
+
+End goal: FinishedAnimations
+
+Template -> ColorMap (program)
+
+ColorMap -> ColorAnimations (manual)
+
+ColorAnimations + ColorMap + Reference -> FinishedAnimations
+
+The ColorAnimations pixels color's map to pixels in the ColorMap.
+
+he ColorMap's pixel positions map to the pixels in the Reference.
+
+The FinalAnimations pixel will be the positions of ColorAnimations 
+and the color of Reference.
+'''
+# Windows
+# ASSET_PATH = "C:\\Users\\brett\\Nextcloud\\projects\\gameart\\Aseprite\\Units\\Generator\\"
+
+# Linux
+# ASSET_PATH = "/home/brettbar/nextcloud/projects/gameart/Aseprite/Units/Generator/"
+
+# WSL
+ASSET_PATH = "/mnt/c/Users/brett/Nextcloud/projects/gameart/Aseprite/Units/Generator/"
+
+def main():
+    gen_color_map()
+    gen_finished_anims()
+
+def gen_finished_anims():
+    color_anims = Image.open(ASSET_PATH + 'MaleAnimations.png').convert('RGBA')
+    color_map = Image.open(ASSET_PATH + 'MaleColorMap.png').convert('RGBA')
+    reference = Image.open(ASSET_PATH + 'GreekVillager.png').convert('RGBA')
+
+    final_anims = color_anims.copy()
+
+    anims_width, anims_height = color_anims.size
+    map_width, map_height =  color_map.size
+
+    top_left_color_anims_pixels = []
+    top_left_color_map_pixels = []
 
 
-for x in range(0, width - 1):
+    # Pixel space
+    # TODO only top left atm
+    for anims_x in range(0, anims_width -1):
+        for anims_y in range(0, anims_height -1):
+            r, g, b, a = color_anims.getpixel((anims_x, anims_y))
+
+            if a > 0: 
+                # print("Adding pixel", (anims_x, anims_y), (r, g, b, a))
+                top_left_color_anims_pixels.append((anims_x, anims_y))
+
+
+    for map_x in range(0, math.floor(map_width /2) -1):
+        for map_y in range(0, math.floor(map_height /2) -1):
+            _, _, _, a = color_map.getpixel((map_x, map_y))
+
+            if a > 0: top_left_color_map_pixels.append((map_x, map_y))
+    
+
+
+    for anims_pixel in top_left_color_anims_pixels:
+        anims_color = color_anims.getpixel(anims_pixel)
+
+
+        for map_pixel in top_left_color_map_pixels:
+            map_color = color_map.getpixel(map_pixel)
+
+            
+            if map_color == anims_color:
+                matching_pixel = map_pixel
+
+                final_anims.putpixel(anims_pixel, reference.getpixel(matching_pixel))
+
+            
+    final_anims.save(ASSET_PATH + "MaleFinalAnimations.png")
+            
+                    
+
+
+def gen_color_map():
+
+    template_image = Image.open(ASSET_PATH + 'MaleTemplate.png')
+
+    template_pic = template_image.convert('RGBA')
+    r, _, _, _ = template_image.split()
+    width, height = template_pic.size
+
+    if not width == template_pic.size[0] or not height == template_pic.size[1]:
+        print("Incompatible files")
+        return
+
+    # hues = {
+    #     "right_arm": (250, 0, 0),
+    #     "head": (225, 0, 0),
+    #     "left_arm": (200, 0, 0),
+    #     "torso": (175, 0, 0),
+    #     "right_leg": (150, 0, 0),
+    #     "left_leg": (125, 0, 0),
+    #     "right_equip": (100, 0, 0),
+    #     "left_equip": (75, 0, 0),
+    #     "cape": (50, 0, 0),
+    #     "mount": (25, 0, 0),
+    #     #"shadow": (70, 0, 0),
+    # }
+
+    
+    sprites = {
+        "TopLeft": {
+            "right_arm": [],
+            "head": [],
+            "left_arm": [],
+            "torso": [],
+            "right_leg": [],
+            "left_leg": [],
+            "right_equipped": [],
+            "left_equip": [],
+            "cape": [],
+            "mount": [],
+        },
+        "TopRight": {
+            "right_arm": [],
+            "head": [],
+            "left_arm": [],
+            "torso": [],
+            "right_leg": [],
+            "left_leg": [],
+            "right_equipped": [],
+            "left_equip": [],
+            "cape": [],
+            "mount": [],
+        }
+    }
+
+
+    
     for y in range(0, height -1):
-        r,g,b,a = picture.getpixel( (x,y) )
-        if r==254 and g==254 and b==254:
-            new_color = (225, 47, 47, 255)
-            picture.putpixel( (x,y), new_color)
-        if r==236 and g==236 and b==236:
-            new_color = (190, 27, 27, 255)
-            picture.putpixel( (x,y), new_color)
-        if r==216 and g==216 and b==216:
-            new_color = (152, 22, 22, 255)
-            picture.putpixel( (x,y), new_color)
+        for x in range(0, width -1):
+            r, g, b, a = template_pic.getpixel((x,y))
 
-picture = picture.save("new.png")
+            if a == 0: continue
+
+            direction = ""
+            if x < 128:
+                direction = "TopLeft"
+            elif x >= 128:
+                direction = "TopRight"
+
+            if r == 255 and g == 255 and b == 255:
+                sprites[direction]["right_arm"].append((x, y))
+            elif r == 255 and g == 0 and b == 0:
+                sprites[direction]["head"].append((x, y))
+            elif r == 255 and g == 128 and b == 0:
+                sprites[direction]["left_arm"].append((x, y))
+            elif r == 255 and g == 255 and b == 0:
+                sprites[direction]["torso"].append((x, y))
+            elif r == 0 and g == 255 and b == 0:
+                sprites[direction]["right_leg"].append((x, y))
+            elif r == 0 and g == 0 and b == 255:
+                sprites[direction]["left_leg"].append((x, y))
+            elif r == 128 and g == 255 and b == 255:
+                sprites[direction]["right_equip"].append((x, y))
+            elif r == 255 and g == 128 and b == 255:
+                sprites[direction]["left_equip"].append((x, y))
+            elif r == 128 and g == 128 and b == 128:
+                sprites[direction]["cape"].append((x, y))
+            elif r == 255 and g == 128 and b == 128:
+                sprites[direction]["mount"].append((x, y))
+
+    for sprite in sprites.values():
+        for body_part in sprite.values():
+
+            if len(body_part) <= 0: continue
+
+
+            prev_x, prev_y = body_part[0]
+
+            for x,y in body_part:
+                (pr, pg, pb, pa) = template_pic.getpixel((prev_x, prev_y))
+
+                pr -= 2
+                pg -= 2
+                pb -= 2
+
+                # if x > prev_x: # we moved a column over
+                #     pr -= 1
+                #     pg -= 2
+
+                # if y > prev_y:  # we moved a row down
+                #     pr -= 1
+                #     pb -= 2
+                    
+                template_pic.putpixel((x, y), (pr, pg, pb, pa))
+
+                prev_x = x
+                prev_y = y
+                 
+
+    # picture.putalpha(alpha)
+    template_pic.save(ASSET_PATH + "MaleColorMap.png")
+
+
+
+
+if __name__ == "__main__":
+   main() 
