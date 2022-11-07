0440a23f810c876941b14ac12e0f5cc7e511eaf2
Brett <brettbar@live.com>
2022-11-06 19:28:25 -0800

    just gonna do json for data, way faster thanyaml

---
+++ src/systems/resource_system.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace ResourceSystem {
+
+/*
+TODO this probably should be moved to component
+*/
+enum class ResourceType {
+  RawMaterial,
+  ProcessedMaterial,
+  Product,
+};
+struct Resource {
+  std::string name;
+  ResourceType type;
+  // Recipes
+};
+
+const std::string raw_materials = {};
+const std::string processed_materials = {};
+const std::string products = {};
+
+struct RawMatRecipe {
+  std::string resource;
+  std::vector<std::string> inputs;
+  std::string building;
+  std::string biome;
+};
+
+struct ProcMatRecipe {};
+
+inline std::map<std::string, std::vector<RawMatRecipe>> recipes;
+
+// MyClass data("hello", 6, true);
+// std::stringstream os;
+// {
+//     cereal::JSONOutputArchive archive_out(os);
+//     archive_out(CEREAL_NVP(data));
+// }
+// string json_str = os.str();
+// cout << json_str << endl;
+
+// // deserialize
+// std::stringstream is(json_str);
+// MyClass data_new;
+// {
+//     cereal::JSONInputArchive archive_in(is);
+//     archive_in(data_new);
+//     cout << data_new.y << endl;
+// }
+
+};// namespace ResourceSystem
