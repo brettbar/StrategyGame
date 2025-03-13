c06ed8141312018ea1caa418086fe2c783701506
Brett Barinaga <brettbar@live.com>
2025-03-13 09:36:45 -0700

    good progress on sprgen2

--- tools/sprgen_v2/main.py
+++ tools/sprgen_v2/main.py
@@ -2,6 +2,8 @@
 
 from PIL import Image, ImageDraw
 
+archetypes_dir = './Archetypes/'
+
 
 def main():
 
@@ -12,14 +14,8 @@ def main():
         gen_archetype_gradient(archetype)
 
 
-
-
 def gen_archetype_gradient(archetype):
-    sprite_w = 128
-    sprite_h = 128
-    archetypes_dir = './Archetypes/'
     silhouette_layers_dir = archetypes_dir + archetype + '/' + '2_SilhouetteLayers/'
-    gradient_layer_output = archetypes_dir + archetype + '/' + '3_GradientLayers/'
 
     # @future might replace this
     supported_layers= [
@@ -43,49 +39,68 @@ def gen_archetype_gradient(archetype):
 	        silhouette_layers_dir + supported_layer + '.png'
 	    ).convert('RGBA')
 
+    sprite_w = 128
+    sprite_h = 128
+    start_x = 0
+    end_x = sprite_w
+    start_y = 0
+    end_y = sprite_h
+
     # gen gradient for each individual layer of the silhouette
     for silhouette, img in silhouettes.items():
-        gradient_img = Image.new('RGBA', (sprite_w, sprite_h), (0, 0, 0, 0))
-        silhouette_arr = []
+        silhouette_arr = gen_silhouette_arr(img, 0, sprite_w, 0, sprite_h)
 
-        for y in range(sprite_h):
-            for x in range(sprite_w):
-                pixel = img.getpixel((x, y))
-                if pixel[3] == 255: 
-                    silhouette_arr.append(pixel)
-        
         original_color = silhouette_arr[0]
-        r, g, b, a = original_color
-
-        # instead of interp with black, I use half the original color since
-        # it makes the darker parts of each layer easier to distinguish from 
-        # one another
-        end_color = (r/2, g/2, b/2, a)
-        r2, g2, b2, _ = end_color
 
         num_pixels = len(silhouette_arr)
-        gradient = []
+        gradient = gen_gradient_arr(original_color, num_pixels)
 
-        for i in range(num_pixels):
-            factor = (num_pixels - i - 1) / (num_pixels - 1)
-
-            new_r = int(r + (r2 - r) * factor)
-            new_g = int(g + (g2 - g) * factor)
-            new_b = int(b + (b2 - b) * factor)
-
-            gradient.append((new_r, new_g, new_b, a))
-
-
-        index = 0
-        for y in range(sprite_h):
-            for x in range(sprite_w):
-                pixel = img.getpixel((x, y))
-                if pixel[3] == 255: 
-                    gradient_img.putpixel((x,y), gradient[index])
-                    index += 1
-
+        gradient_img = gen_gradient_img(img, gradient, start_x, end_x, start_y, end_y)
+        gradient_layer_output = archetypes_dir + archetype + '/' + '3_GradientLayers/'
         gradient_img.save(gradient_layer_output + silhouette + '.png')
 
+def gen_silhouette_arr(img, start_x, end_x, start_y, end_y):
+    silhouette_arr = []
+    for y in range(start_y, end_y):
+        for x in range(start_x, end_x):
+            pixel = img.getpixel((x, y))
+            if pixel[3] == 255: 
+                silhouette_arr.append(pixel)
+    return silhouette_arr
+
+def gen_gradient_arr(original_color, num_pixels):
+    r, g, b, a = original_color
+
+    # instead of interp with black, I use half the original color since
+    # it makes the darker parts of each layer easier to distinguish from 
+    # one another
+    end_color = (r/2, g/2, b/2, a)
+    r2, g2, b2, _ = end_color
+
+    gradient = []
+
+    for i in range(num_pixels):
+        factor = (num_pixels - i - 1) / (num_pixels - 1)
+
+        new_r = int(r + (r2 - r) * factor)
+        new_g = int(g + (g2 - g) * factor)
+        new_b = int(b + (b2 - b) * factor)
+
+        gradient.append((new_r, new_g, new_b, a))
+
+    return gradient
+
+
+def gen_gradient_img(img, gradient, start_x, end_x, start_y, end_y):
+    gradient_img = Image.new('RGBA', (end_x - start_x, end_y - start_y), (0, 0, 0, 0))
+    index = 0
+    for y in range(start_y, end_y):
+        for x in range(start_x, end_x):
+            pixel = img.getpixel((x, y))
+            if pixel[3] == 255: 
+                gradient_img.putpixel((x,y), gradient[index])
+                index += 1
+    return gradient_img
 
 
 if __name__ == "__main__":
