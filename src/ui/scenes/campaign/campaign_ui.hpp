e0422bf45a19c83bf3c0b964d17ca369d73da9e8
Brett Barinaga <brettbar@live.com>
2025-05-20 23:13:59 -0700

    asdasd

--- src/ui/scenes/campaign/campaign_ui.hpp
+++ src/ui/scenes/campaign/campaign_ui.hpp
@@ -10,30 +10,30 @@
 
 namespace UI {
 
-  inline Commands::Command settlement_context( entt::entity player_e ) {
+  inline Commands::Command settlement_context(entt::entity player_e) {
     auto settlement =
       Selection::System::GetSelectedComponent<Settlement::Component>();
     auto prov = Selection::System::GetSelectedComponent<Province::Component>();
     // entt::entity player_e = GetLocalPlayerE();
-    auto faction = Global::world.get<Faction::Component>( player_e );
+    auto faction = Global::world.get<Faction::Component>(player_e);
 
     // if ( !settlement || !prov ) {
     //   printf( "Got null prov/settlement??\n" );
     //   return nullptr;
     // }
 
-    auto action = UI::settlement_context( settlement );
-    switch ( action ) {
+    auto action = UI::settlement_context(settlement);
+    switch (action) {
       case UI::Action_SettlementContext::None:
         break;
       case UI::Action_SettlementContext::SpawnColonist:
         return Commands::Command::spawn_colonist(
-          player_e, Settlement::System::settlement_position( *prov )
+          player_e, Settlement::System::settlement_position(*prov)
         );
         break;
       case UI::Action_SettlementContext::SpawnArmy:
         return Commands::Command::spawn_army(
-          player_e, Settlement::System::settlement_position( *prov )
+          player_e, Settlement::System::settlement_position(*prov)
         );
         break;
         // case UI::Action_SettlementContext::BuildFarm:
@@ -55,9 +55,9 @@ namespace UI {
     // }
 
     // auto action = UI::ActorContext( actor );
-    auto action = UI::actor_context( actor );
+    auto action = UI::actor_context(actor);
 
-    switch ( action ) {
+    switch (action) {
       case UI::Action_ActorContext::None:
         break;
       case UI::Action_ActorContext::ClaimProvince: {
@@ -87,14 +87,14 @@ namespace UI {
     Buildings::BuildingType building;
   };
 
-  inline Action_Campaign campaign_ui( entt::entity player_e ) {
+  inline Action_Campaign campaign_ui(entt::entity player_e) {
 
     // @todo refactor this shit
     opt<Buildings::BuildingType> building_to_build = std::nullopt;
     Commands::Command cmd = Commands::Command::none();
 
-    CLAY( {
-      .id = CLAY_ID( "Campaign" ),
+    CLAY({
+      .id = CLAY_ID("Campaign"),
       .layout =
         {
           .sizing =
@@ -110,15 +110,15 @@ namespace UI {
             },
           .layoutDirection = CLAY_TOP_TO_BOTTOM,
         },
-    } ) {
-      CLAY( {
-        .id = CLAY_ID( "Campaign::TopRow" ),
+    }) {
+      CLAY({
+        .id = CLAY_ID("Campaign::TopRow"),
         .layout =
           {
             .sizing =
               {
                 .width = CLAY_SIZING_GROW(),
-                .height = CLAY_SIZING_PERCENT( 0.6 ),
+                .height = CLAY_SIZING_PERCENT(0.6),
               },
             .childAlignment =
               {
@@ -127,11 +127,11 @@ namespace UI {
               },
             .layoutDirection = CLAY_LEFT_TO_RIGHT,
           },
-      } ) {
+      }) {
         tab = UI::overview_tabs();
 
-        CLAY( {
-          .id = CLAY_ID( "Campaign::TopRow::LeftCol" ),
+        CLAY({
+          .id = CLAY_ID("Campaign::TopRow::LeftCol"),
           .layout =
             {
               .sizing =
@@ -142,13 +142,13 @@ namespace UI {
               // .childGap = 8,
               .layoutDirection = CLAY_TOP_TO_BOTTOM,
             },
-        } ) {
-          UI::top_bar( 1, 0 );
+        }) {
+          UI::top_bar(1, 0);
 
-          if ( std::string( tab.chars ) != "" ) {
-            UI::OverviewAction action = UI::overview_content( tab );
+          if (std::string(tab.chars) != "") {
+            UI::OverviewAction action = UI::overview_content(tab);
 
-            switch ( action.type ) {
+            switch (action.type) {
               case UI::OverviewAction_t::None:
                 break;
               case UI::OverviewAction_t::Construction:
@@ -160,8 +160,8 @@ namespace UI {
         }
       }
 
-      CLAY( {
-        .id = CLAY_ID( "Campaign::BottomRow" ),
+      CLAY({
+        .id = CLAY_ID("Campaign::BottomRow"),
         .layout =
           {
             .sizing =
@@ -176,14 +176,14 @@ namespace UI {
               },
             .layoutDirection = CLAY_LEFT_TO_RIGHT,
           },
-      } ) {
-        CLAY( {
-          .id = CLAY_ID( "Campaign::BottomRow::LeftCol" ),
+      }) {
+        CLAY({
+          .id = CLAY_ID("Campaign::BottomRow::LeftCol"),
           .layout =
             {
               .sizing =
                 {
-                  .width = CLAY_SIZING_PERCENT( 0.33 ),
+                  .width = CLAY_SIZING_PERCENT(0.33),
                   .height = CLAY_SIZING_GROW(),
                 },
               .childAlignment =
@@ -192,26 +192,26 @@ namespace UI {
                 },
               .layoutDirection = CLAY_TOP_TO_BOTTOM,
             },
-        } ) {
-          if ( Selection::System::
-                 Selected<Province::Component, Settlement::Component>() ) {
-            cmd = settlement_context( player_e );
+        }) {
+          if (Selection::System::
+                Selected<Province::Component, Settlement::Component>()) {
+            cmd = settlement_context(player_e);
           }
 
-          if ( Selection::System::Selected<Actor::Component>() ) {
+          if (Selection::System::Selected<Actor::Component>()) {
             cmd = actor_context();
           }
         }
 
         UI::spacer();
 
-        CLAY( {
-          .id = CLAY_ID( "Campaign::RightCol" ),
+        CLAY({
+          .id = CLAY_ID("Campaign::RightCol"),
           .layout =
             {
               .sizing =
-                { .width = CLAY_SIZING_PERCENT( 0.33 ),
-                  .height = CLAY_SIZING_GROW() },
+                {.width = CLAY_SIZING_PERCENT(0.33),
+                 .height = CLAY_SIZING_GROW()},
               .childAlignment =
                 {
                   .x = CLAY_ALIGN_X_RIGHT,
@@ -219,9 +219,9 @@ namespace UI {
                 },
               .layoutDirection = CLAY_TOP_TO_BOTTOM,
             },
-        } ) {
+        }) {
 
-          switch ( UI::minimap() ) {
+          switch (UI::minimap()) {
             case UI::Action_Minimap::Default:
               Map::Manager()->mode = Map::Mode::Default;
               break;
@@ -241,15 +241,15 @@ namespace UI {
       }
     }
 
-    if ( cmd.type != Commands::Type::None ) {
-      return Action_Campaign{ .type = Action_Campaign_t::Command, .cmd = cmd };
-    } else if ( building_to_build.has_value() ) {
+    if (cmd.type != Commands::Type::None) {
+      return Action_Campaign{.type = Action_Campaign_t::Command, .cmd = cmd};
+    } else if (building_to_build.has_value()) {
       return Action_Campaign{
         .type = Action_Campaign_t::SelectBuilding,
         .building = building_to_build.value()
       };
     }
 
-    return Action_Campaign{ .type = Action_Campaign_t::None };
+    return Action_Campaign{.type = Action_Campaign_t::None};
   }
 }// namespace UI
--- src/ui/scenes/campaign/overview_panel/overview_tabs.hpp
+++ src/ui/scenes/campaign/overview_panel/overview_tabs.hpp
@@ -10,7 +10,7 @@
 namespace UI {
 
   inline int _overview_panel_i = 0;
-  inline Clay_String tab = CLAY_STRING( "" );
+  inline Clay_String tab = CLAY_STRING("");
 
 
   inline Clay_String overview_tabs() {
@@ -21,83 +21,83 @@ namespace UI {
 
     Tab tabs[] = {
       Tab{
-        .ref = CLAY_STRING( "Overview" ),
-        .icon = hstr{ "rome_banner.png" },
+        .ref = CLAY_STRING("Overview"),
+        .icon = hstr{"rome_banner.png"},
       },
       Tab{
-        .ref = CLAY_STRING( "Diplomacy" ),
-        .icon = hstr{ "diplomacy_tab.png" },
+        .ref = CLAY_STRING("Diplomacy"),
+        .icon = hstr{"diplomacy_tab.png"},
       },
       Tab{
-        .ref = CLAY_STRING( "Resources" ),
-        .icon = hstr{ "resources_tab.png" },
+        .ref = CLAY_STRING("Resources"),
+        .icon = hstr{"resources_tab.png"},
       },
       Tab{
-        .ref = CLAY_STRING( "Construction" ),
-        .icon = hstr{ "construction_tab.png" },
+        .ref = CLAY_STRING("Construction"),
+        .icon = hstr{"construction_tab.png"},
       },
       Tab{
-        .ref = CLAY_STRING( "Agents" ),
-        .icon = hstr{ "agents_tab.png" },
+        .ref = CLAY_STRING("Agents"),
+        .icon = hstr{"agents_tab.png"},
       },
       Tab{
-        .ref = CLAY_STRING( "Military" ),
-        .icon = hstr{ "military_tab.png" },
+        .ref = CLAY_STRING("Military"),
+        .icon = hstr{"military_tab.png"},
       },
       Tab{
-        .ref = CLAY_STRING( "Innovations" ),
-        .icon = hstr{ "long_slot.png" },
+        .ref = CLAY_STRING("Innovations"),
+        .icon = hstr{"long_slot.png"},
       },
       Tab{
-        .ref = CLAY_STRING( "Laws & Customs" ),
-        .icon = hstr{ "long_slot.png" },
+        .ref = CLAY_STRING("Laws & Customs"),
+        .icon = hstr{"long_slot.png"},
       },
-      // Tab{
-      //   .ref = CLAY_STRING( "Missions" ),
-      //   .icon = hstr{ "long_slot.png" },
-      // },
+      Tab{
+        .ref = CLAY_STRING("Missions"),
+        .icon = hstr{"long_slot.png"},
+      },
       // Tab{
       //   .ref = CLAY_STRING( "Objectives" ),
       //   .icon = hstr{ "long_slot.png" },
       // },
     };
-    u32 num_tabs = LEN( tabs );
+    u32 num_tabs = LEN(tabs);
 
-    CLAY( {
-      .id = CLAY_ID( "OverviewPanel" ),
+    CLAY({
+      .id = CLAY_ID("OverviewPanel"),
       .layout =
         {
-          .sizing = { .width = CLAY_SIZING_FIT( 128 ) },
+          .sizing = {.width = CLAY_SIZING_FIT(128)},
           .childGap = 4,
           .layoutDirection = CLAY_TOP_TO_BOTTOM,
         },
-        .backgroundColor = COLOR_BLACK,
-    } ) {
-      for ( u32 i = 0; i < num_tabs; i++ ) {
-        vec2f dims = { 66 , 32 };
-        if ( std::string( tabs[i].ref.chars ) == "Overview" ) {
-          dims = { 66, 64 };
+      .backgroundColor = COLOR_BLACK,
+    }) {
+      for (u32 i = 0; i < num_tabs; i++) {
+        vec2f dims = {66, 32};
+        if (std::string(tabs[i].ref.chars) == "Overview") {
+          dims = {66, 64};
         }
 
-        CLAY( {
-          .id = CLAY_IDI( "OverviewPanel::tab", i ),
+        CLAY({
+          .id = CLAY_IDI("OverviewPanel::tab", i),
           .layout =
             {
               .sizing =
                 {
-                  .width = CLAY_SIZING_FIXED( dims.x * UI_SCALE ),
+                  .width = CLAY_SIZING_FIXED(dims.x * UI_SCALE),
                 },
-              .childAlignment = { CLAY_ALIGN_X_CENTER },
+              .childAlignment = {CLAY_ALIGN_X_CENTER},
             },
-        } ) {
-          texture_label( tabs[i].icon, dims );
+        }) {
+          texture_label(tabs[i].icon, dims);
         }
       }
     }
 
 
-    for ( u32 i = 0; i < num_tabs; i++ ) {
-      if ( ButtonWasClicked( CLAY_STRING( "OverviewPanel::tab" ), i ) ) {
+    for (u32 i = 0; i < num_tabs; i++) {
+      if (ButtonWasClicked(CLAY_STRING("OverviewPanel::tab"), i)) {
         // return tab.action;
         _overview_panel_i = i;
         tab = tabs[_overview_panel_i].ref;
@@ -105,8 +105,8 @@ namespace UI {
       }
     }
 
-    if ( ButtonWasClicked( CLAY_STRING( "Exit" ) ) ) {
-      tab = CLAY_STRING( "" );
+    if (ButtonWasClicked(CLAY_STRING("Exit"))) {
+      tab = CLAY_STRING("");
     }
 
     return tab;
