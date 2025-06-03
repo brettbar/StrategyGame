988bf76498f914d4734346a40c244c9c7d4ec3b6
Brett Barinaga <brettbar@live.com>
2025-06-03 07:08:50 -0700

    asds

--- src/ui/scenes/campaign/overview_panel/construction_tab.hpp
+++ src/ui/scenes/campaign/overview_panel/construction_tab.hpp
@@ -144,6 +144,87 @@ namespace UI {
     Build
   };
 
+  inline void construction_preview_top_row(Building building) {
+    CLAY({
+      .id = CLAY_ID("Construction::Preview::TopRow"),
+      .layout = {.childGap = 8},
+    }) {
+      texture_button(
+        CLAY_STRING("Construction::Preview::Back"),
+        hstr{"back_button.png"},
+        {15, 15}
+      );
+
+      CLAY({
+        .layout =
+          {
+            .childGap = 8,
+            .childAlignment =
+              {
+                .y = CLAY_ALIGN_Y_CENTER,
+              },
+          },
+      }) {
+
+        texture_label(hstr{building.path.c_str()}, {32, 32});
+
+        text_label(building.label, 16);
+      }
+    }
+  }
+
+  inline void construction_preview_middle_row() {
+    CLAY({
+      .id = CLAY_ID("Construction::Preview::Desc"),
+      .layout =
+        {
+          .sizing =
+            {
+              .width = CLAY_SIZING_FIXED(256 * UI_SCALE),
+            },
+        },
+    }) {
+      text_label(
+        CLAY_STRING("Description of a farm. Description of a farm. "
+                    "Description of a farm."),
+        8
+      );
+    }
+
+
+    CLAY({.id = CLAY_ID("Construction::Preview::MiddleRow")}) {
+      CLAY() {
+        text_label(CLAY_STRING("Produce:"), 12);
+        // @leftoff Pull in recipes for building
+      }
+
+      CLAY() {
+        text_label(CLAY_STRING("Requires:"), 12);
+      }
+    }
+  }
+
+  inline void construction_preview_bottom_row() {
+    CLAY({
+      .id = CLAY_ID("Construction::Preview::BottomRow"),
+      .layout =
+        {
+          .sizing =
+            {
+              .width = CLAY_SIZING_GROW(),
+            },
+          .childAlignment =
+            {
+              .x = CLAY_ALIGN_X_RIGHT,
+            },
+        },
+    }) {
+      text_button_small(
+        CLAY_STRING("Construction::Build"), CLAY_STRING("Build"), 0
+      );
+    }
+  }
+
   inline Action_ConstructionPreview construction_preview(Building building) {
     CLAY({
       .id = CLAY_ID("Construction::Preview"),
@@ -158,69 +239,13 @@ namespace UI {
         },
     }) {
 
-      CLAY({
-        .id = CLAY_ID("Construction::Preview::TopRow"),
-        .layout = {.childGap = 8},
-      }) {
-        texture_button(
-          CLAY_STRING("Construction::Preview::Back"),
-          hstr{"back_button.png"},
-          {15, 15}
-        );
+      construction_preview_top_row(building);
 
-        CLAY({
-          .layout =
-            {
-              .childGap = 8,
-              .childAlignment =
-                {
-                  .y = CLAY_ALIGN_Y_CENTER,
-                },
-            },
-        }) {
+      construction_preview_middle_row();
 
-          texture_label(hstr{building.path.c_str()}, {32, 32});
-
-          text_label(building.label, 16);
-        }
-      }
-
-      CLAY({
-        .id = CLAY_ID("Construction::Preview::Desc"),
-        .layout =
-          {
-            .sizing =
-              {
-                .width = CLAY_SIZING_FIXED(256 * UI_SCALE),
-              },
-          },
-      }) {
-        text_label(
-          CLAY_STRING("Description of a farm. Description of a farm. "
-                      "Description of a farm."),
-          8
-        );
-      }
-      CLAY(CLAY_ID("Construction::Preview::MiddleRow")) {}
       UI::spacer();
-      CLAY({
-        .id = CLAY_ID("Construction::Preview::BottomRow"),
-        .layout =
-          {
-            .sizing =
-              {
-                .width = CLAY_SIZING_GROW(),
-              },
-            .childAlignment =
-              {
-                .x = CLAY_ALIGN_X_RIGHT,
-              },
-          },
-      }) {
-        text_button_small(
-          CLAY_STRING("Construction::Build"), CLAY_STRING("Build"), 0
-        );
-      }
+
+      construction_preview_bottom_row();
     }
 
 
@@ -234,6 +259,7 @@ namespace UI {
 
     return Action_ConstructionPreview::None;
   }
+
 
   inline opt<Buildings::BuildingType> construction_tab() {
 
