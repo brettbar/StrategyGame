5433bc3b63ff24225a6d2a6e08763d51ebfc2918
Brett Barinaga <brettbar@live.com>
2025-06-03 09:18:29 -0700

    working on build preview

---
+++ src/ui/scenes/campaign/overview_panel/construction/construction_preview.hpp
@@ -0,0 +1,152 @@
+#pragma once
+
+#include "../../../../../../data/buildings.hpp"
+#include "../../../../library/text_button.hpp"
+#include "../../../../library/text_label.hpp"
+#include "../../../../library/texture_button.hpp"
+
+
+namespace UI {
+
+  // @todo this should be coming from data/ at somepoint
+  struct Building {
+    Buildings::BuildingType name;
+    Buildings::BuildingCategory category;
+    Clay_String label;
+    str path;
+  };
+
+  enum class Action_ConstructionPreview {
+    None,
+    Back,
+    Build
+  };
+
+  inline void construction_preview_top_row(Building building) {
+    CLAY({
+      .id = CLAY_ID("Construction::Preview::TopRow"),
+      .layout = {.childGap = 8},
+    }) {
+      texture_button(
+        CLAY_STRING("Construction::Preview::Back"),
+        hstr{"back_button.png"},
+        {15, 15}
+      );
+
+      CLAY({
+        .layout =
+          {
+            .childGap = 8,
+            .childAlignment =
+              {
+                .y = CLAY_ALIGN_Y_CENTER,
+              },
+          },
+      }) {
+
+        texture_label(hstr{building.path.c_str()}, {32, 32});
+
+        text_label(building.label, 16);
+      }
+    }
+  }
+
+  inline void construction_preview_middle_row(Building building) {
+
+    list<Buildings::Recipe> recipes =
+      Buildings::recipes_for_building(building.name);
+
+    CLAY({
+      .id = CLAY_ID("Construction::Preview::Desc"),
+      .layout =
+        {
+          .sizing =
+            {
+              .width = CLAY_SIZING_FIXED(256 * UI_SCALE),
+            },
+        },
+    }) {
+      text_label(
+        CLAY_STRING("Description of a farm. Description of a farm. "
+                    "Description of a farm."),
+        8
+      );
+    }
+
+
+    CLAY({.id = CLAY_ID("Construction::Preview::MiddleRow")}) {
+      CLAY() {
+        text_label(CLAY_STRING("Produce:"), 12);
+
+        for (const Buildings::Recipe &recipe: recipes) {
+          CLAY() {
+            for (const Buildings::RecipeItem &item: recipe.outputs) {
+              texture_label(hstr{ResourceStr(item.resource)}, {32, 32});
+            }
+          }
+        }
+      }
+
+      CLAY() {
+        text_label(CLAY_STRING("Requires:"), 12);
+      }
+    }
+  }
+
+  inline void construction_preview_bottom_row() {
+    CLAY({
+      .id = CLAY_ID("Construction::Preview::BottomRow"),
+      .layout =
+        {
+          .sizing =
+            {
+              .width = CLAY_SIZING_GROW(),
+            },
+          .childAlignment =
+            {
+              .x = CLAY_ALIGN_X_RIGHT,
+            },
+        },
+    }) {
+      text_button_small(
+        CLAY_STRING("Construction::Build"), CLAY_STRING("Build"), 0
+      );
+    }
+  }
+
+  inline Action_ConstructionPreview construction_preview(Building building) {
+    CLAY({
+      .id = CLAY_ID("Construction::Preview"),
+      .layout =
+        {
+          .sizing =
+            {
+              .width = CLAY_SIZING_GROW(),
+              .height = CLAY_SIZING_GROW(),
+            },
+          .layoutDirection = CLAY_TOP_TO_BOTTOM,
+        },
+    }) {
+
+      construction_preview_top_row(building);
+
+      construction_preview_middle_row(building);
+
+      UI::spacer();
+
+      construction_preview_bottom_row();
+    }
+
+
+    if (ButtonWasClicked(CLAY_STRING("Construction::Preview::Back"))) {
+      return Action_ConstructionPreview::Back;
+    }
+
+    if (ButtonWasClicked(CLAY_STRING("Construction::Build"))) {
+      return Action_ConstructionPreview::Build;
+    }
+
+    return Action_ConstructionPreview::None;
+  }
+
+}// namespace UI
--- src/ui/scenes/campaign/overview_panel/overview_content.hpp
+++ src/ui/scenes/campaign/overview_panel/overview_content.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 #define CLAY_EXTEND_CONFIG_IMAGE hstr texture_id;
-#include "construction_tab.hpp"
-#include "resources_tab.hpp"
 #include "../../../library/texture_button.hpp"
+#include "construction/construction_tab.hpp"
+#include "resources_tab.hpp"
+
 
 namespace UI {
   enum class OverviewAction_t {
@@ -15,33 +16,33 @@ namespace UI {
     Buildings::BuildingType building;
   };
 
-  inline OverviewAction overview_content( Clay_String current_tab ) {
+  inline OverviewAction overview_content(Clay_String current_tab) {
     opt<Buildings::BuildingType> building_to_build = std::nullopt;
 
-    CLAY( {
-      .id = CLAY_ID( "OverviewPanel::Content" ),
+    CLAY({
+      .id = CLAY_ID("OverviewPanel::Content"),
       .layout =
         {
           .sizing =
             {
-              .width = CLAY_SIZING_FIXED( 243 * UI_SCALE ),
+              .width = CLAY_SIZING_FIXED(243 * UI_SCALE),
               .height = CLAY_SIZING_GROW(),
             },
-          .padding = { 16, 16 },
+          .padding = {16, 16},
           .childGap = 8,
           .layoutDirection = CLAY_TOP_TO_BOTTOM,
         },
       .backgroundColor = COLOR_TRANSPARENT_BLACK,
       // .cornerRadius = { 5, 5, 5, 5 },
-    } ) {
-      CLAY( {
-        .id = CLAY_ID( "OverviewPanel::Content::Banner" ),
+    }) {
+      CLAY({
+        .id = CLAY_ID("OverviewPanel::Content::Banner"),
         .layout =
           {
             .sizing =
               {
                 .width = CLAY_SIZING_GROW(),
-                .height = CLAY_SIZING_FIT( 24 * UI_SCALE ),
+                .height = CLAY_SIZING_FIT(24 * UI_SCALE),
               },
             .childAlignment =
               {
@@ -49,42 +50,42 @@ namespace UI {
                 .y = CLAY_ALIGN_Y_CENTER,
               },
           },
-      } ) {
+      }) {
         CLAY() {
           CLAY_TEXT(
             current_tab,
-            CLAY_TEXT_CONFIG( {
+            CLAY_TEXT_CONFIG({
               .textColor = COLOR_WHITE,
-              .fontSize = uint16_t( 12 * UI_SCALE ),
-            } )
+              .fontSize = uint16_t(12 * UI_SCALE),
+            })
           );
         }
 
-        CLAY( {
+        CLAY({
           .layout =
             {
-              .sizing = { CLAY_SIZING_GROW() },
+              .sizing = {CLAY_SIZING_GROW()},
             },
-        } );
-        
+        });
+
         texture_button(CLAY_STRING("Exit"), hstr{"exit.png"}, {15, 15});
       }
 
-      if ( std::string( current_tab.chars ) == "Construction" ) {
+      if (std::string(current_tab.chars) == "Construction") {
         building_to_build = UI::construction_tab();
-      } else if ( std::string( current_tab.chars ) == "Resources" ) {
+      } else if (std::string(current_tab.chars) == "Resources") {
         UI::resources_tab();
       }
     }
 
 
-    if ( building_to_build.has_value() ) {
+    if (building_to_build.has_value()) {
       return OverviewAction{
         .type = OverviewAction_t::Construction,
         .building = building_to_build.value(),
       };
     }
 
-    return { .type = OverviewAction_t::None };
+    return {.type = OverviewAction_t::None};
   }
 }// namespace UI
